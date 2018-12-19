3954a4f362c0192031860a7e5c7a357f628fbe23
Logan Murphy <nagolyhprum@hotmail.com>
2018-12-19 18:19:02 -0500

    add broken move function

--- __tests__/snake.js
+++ __tests__/snake.js
@@ -27,4 +27,15 @@ describe('snake', () => {
       y: 5
     })
   })
+  it('moves', () => {
+    const snake = new Snake({
+      columns: 12,
+      rows: 10
+    })
+    snake.move()
+    expect(snake.data.body[0].data).toEqual({
+      x: 6,
+      y: 4
+    })
+  })
 })
