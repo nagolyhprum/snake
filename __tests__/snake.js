357aa41888301fe4c4cb2985e6d40095730f9e58
Logan Murphy <nagolyhprum@hotmail.com>
2018-12-19 18:38:25 -0500

    make sure tail follows

--- __tests__/snake.js
+++ __tests__/snake.js
@@ -120,4 +120,46 @@ describe('snake', () => {
       skip: false
     })
   })
+  it('tail follows head', () => {
+    const snake = new Snake({
+      columns: 12,
+      rows: 10
+    })
+    snake.grow()
+    expect(snake.data.body[1].data).toEqual({
+      x: 6,
+      y: 5,
+      skip: true
+    })
+    snake.move(UP)
+    expect(snake.data.body[0].data).toEqual({
+      x: 6,
+      y: 5,
+      skip: false
+    })
+    snake.move(RIGHT)
+    expect(snake.data.body[0].data).toEqual({
+      x: 6,
+      y: 4,
+      skip: false
+    })
+    snake.move(DOWN)
+    expect(snake.data.body[0].data).toEqual({
+      x: 7,
+      y: 4,
+      skip: false
+    })
+    snake.move(LEFT)
+    expect(snake.data.body[0].data).toEqual({
+      x: 7,
+      y: 5,
+      skip: false
+    })
+    snake.move(UP)
+    expect(snake.data.body[0].data).toEqual({
+      x: 6,
+      y: 5,
+      skip: false
+    })
+  })
 })
